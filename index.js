ec0717daeb8c08f0129b82eaa0f66bf3512dbe28
loganhansel <loganhansel23@gmail.com>
2022-05-12 15:29:05 -0400

    commit tree bed82d809104c55bb44a39bd9c47f1514906a14b

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+
+function superbowlWin(record){
+    let winningYear = record.find(i => i.result === 'W');
+    if (typeof winningYear === 'object') {
+        return winningYear.year;
+    } else {
+        return undefined;
+    }
+}
